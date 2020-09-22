3c33ccc2451353a5bc3c33605db7d5788c48b562
Hamill210 <han95210@naver.com>
2020-09-23 01:24:11 +0900

    feat : 모든 Request Header 출력하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,8 +1,10 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
@@ -22,10 +24,29 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
+        try {
+            BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line = bf.readLine();;
+
+            while (!"".equals(line)) {
+                if (line == null) {
+                    System.out.println("line이 null이예요!");
+                    return;
+                }
+                log.info("##### {}", line);
+                line = bf.readLine();
+            }
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = "Hello World".getBytes();
+
+
+
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
