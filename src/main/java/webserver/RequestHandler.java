164f07c92ac510281a4f95df88c6890988ae89a7
Hamill210 <han95210@naver.com>
2020-09-23 02:02:54 +0900

    feat : Request Parameter 추출하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,8 +10,11 @@ import java.io.OutputStream;
 import java.net.Socket;
 
 import java.nio.file.Files;
+import java.util.Map;
+import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -26,8 +29,6 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-
-
         try {
             BufferedReader bf = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String line = bf.readLine();;
@@ -37,8 +38,25 @@ public class RequestHandler extends Thread {
             String url = tokens[urlIndex];
             log.debug("##### url: {}", url);
 
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
-            log.debug("##### byte body: {}", body);
+            if (url.contains("?")) {
+                String[] urlAndQueryString = url.split("\\?");
+                Map<String,String> queryStringMap = HttpRequestUtils.parseQueryString(urlAndQueryString[1]);
+
+                log.debug("##### map: {}", queryStringMap.get("userId"));
+                log.debug("##### map: {}", queryStringMap.get("password"));
+                log.debug("##### map: {}", queryStringMap.get("name"));
+                log.debug("##### map: {}", queryStringMap.get("email"));
+
+                String userId = queryStringMap.get("userId");
+                String password = queryStringMap.get("password");
+                String name = queryStringMap.get("name");
+                String email = queryStringMap.get("email");
+                User user = new User(userId, password, name, email);
+                log.debug("##### user: {}", user.toString());
+            }
+
+//            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+//            log.debug("##### byte body: {}", body);
 
             while (!"".equals(line)) {
                 if (line == null) {
